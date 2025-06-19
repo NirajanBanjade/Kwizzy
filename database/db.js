7a7a33341306ed5fd457409b63980ce1d6c52b19
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-06-19 14:40:39 -0600

    environment variables backed up

--- database/db.js
+++ database/db.js
@@ -3,13 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' }); // adjust based on actual location
 
 //establishing database connection
-const pool =new Pool(
-    {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-        port: 5432,
-    }
-);
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT, // optional but match .env
+  });
+  
 export default pool;
